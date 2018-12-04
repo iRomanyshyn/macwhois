85491d534a04ed8b0243f2550b2c086e364d2c5a
Ihor Romanyshyn <ihor.vapor.romanyshyn@gmail.com>
2018-12-05 01:35:12 +0200

    go to sleep

--- main.py
+++ main.py
@@ -1,28 +1,23 @@
 #!/usr/bin/python3
 
 import requests
+import re
 
 import config
 import strings
 
 # TODO localouifile in user's home with os module for cross platform work
 
+ouiparseregex = r"^([0-9a-f]{6})\s*\(.{7}\)\s*(.+)$"
 
 class OUIData:
     def __init__(self, oui, org):
         self.oui = oui
         self.org = org
 
-    @classmethod
-    def download(cls, url, file):
-        """This method is used to download OUI file to local machine"""
-        cls.url = url
-        cls.file = file
-        try:
-            a = requests.get(cls.url)
-            print(a)
-        except:
-            pass
+    def download(self, url):
+        self.url = url
+        return requests.get(self.url)
 
     def open(self):
         pass
@@ -30,4 +25,6 @@ class OUIData:
 
     # TODO OuiDownloadFailed exception
 
-OUIData.download(config.oui_url, config.oui_file)
+a = OUIData.download(config.oui_url)
+
+print(re.search(ouiparseregex, a.text,'gmi'))
