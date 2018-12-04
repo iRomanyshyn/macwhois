06e6ccd4eebe19e4431af99ff5481f6731acfe72
Ihor Romanyshyn <ihor.vapor.romanyshyn@gmail.com>
2018-12-05 00:28:22 +0200

    classmethod

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
 #!/usr/bin/python3
 
+import requests
+
+import config
+import strings
+
 # TODO localouifile in user's home with os module for cross platform work
 
 
-class OuiData:
+class OUIData:
     def __init__(self, oui, org):
         self.oui = oui
         self.org = org
-        # TODO 'download' method, 'open' method (with pickle or similar), OuiDownloadFailed exception
+
+    @classmethod
+    def download(cls, url, file):
+        """This method is used to download OUI file to local machine"""
+        cls.url = url
+        cls.file = file
+        try:
+            a = requests.get(cls.url)
+            print(a)
+        except:
+            pass
+
+    def open(self):
+        pass
+
+
+    # TODO OuiDownloadFailed exception
+
+OUIData.download(config.oui_url, config.oui_file)
